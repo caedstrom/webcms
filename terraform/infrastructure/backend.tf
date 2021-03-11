695e994d7cdd1f98807bfbf0f096a3c343c18df8
msglover <glover.michael@epa.gov>
2021-03-11 11:01:31 -0500

    Update backend.tf

--- terraform/infrastructure/backend.tf
+++ terraform/infrastructure/backend.tf
@@ -1,6 +1,6 @@
-terraform {
+#terraform {
   # Backend configuration is left unspecified here: there should be an external backend
   # configuration file used on each `terraform init`
   # cf. https://www.terraform.io/docs/backends/config.html#partial-configuration
-  backend "s3" {}
-}
+#  backend "s3" {}
+#}
